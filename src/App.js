1209d41176260f9da5d003068a9860378e16d1aa
Amr Ibrahim <amribrahim1987@hotmail.com>
2021-04-05 18:09:54 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Component } from "react";
 import "./App.css";
+import { Component } from "react";
 import MessageList from "./components/MessageList";
 import Toolbar from "./components/Toolbar";
 import messages from "./apis/messages";
@@ -13,7 +13,7 @@ const flag = {
 class App extends Component {
   state = {
     messages: messages,
-    selection: null,
+    selection: flag.some,
     unreadCount: 0,
     plural: null,
     disabled: false,
@@ -24,94 +24,108 @@ class App extends Component {
     this.countUnreadMessages(this.state.messages);
   }
 
-  toggleStarring = (id) => {
-    const messages = [...this.state.messages];
-    const message = messages.find((message) => message.id === id);
-    message.starred = !message.starred;
-    this.setState({ messages: messages });
-  };
+  updateOverallSelectionFlag(messages) {
+    const statuses = messages.map((message) => message.selected);
+    const allSelected = statuses.every((status) => status);
+    const allUnselected = statuses.every((status) => !status);
+
+    if (allSelected) {
+      this.setState({ selection: flag.all, disabled: false });
+    } else if (allUnselected) {
+      this.setState({ selection: flag.none, disabled: true });
+    } else {
+      this.setState({ selection: flag.some, disabled: false });
+    }
+  }
+
+  countUnreadMessages(messages) {
+    const unreadMessages = messages.filter((message) => !message.read);
+    const unreadCount = unreadMessages.length;
+
+    this.setState({ unreadCount: unreadCount });
+
+    if (unreadCount === 1) {
+      this.setState({ plural: null });
+    } else {
+      this.setState({ plural: "s" });
+    }
+  }
 
   toggleSelection = (id) => {
     const messages = [...this.state.messages];
     const message = messages.find((message) => message.id === id);
     message.selected = !message.selected;
+
     this.setState({ messages: messages });
     this.updateOverallSelectionFlag(messages);
   };
 
   toggleOverallSelection = () => {
     const messages = [...this.state.messages];
+
     if (this.state.selection === flag.all) {
       messages.forEach((message) => (message.selected = false));
       this.setState({ messages: messages });
-    } else if (
-      this.state.selection === flag.none ||
-      this.state.selection === flag.some
-    ) {
+    } else {
       messages.forEach((message) => (message.selected = true));
       this.setState({ messages: messages });
     }
+
     this.updateOverallSelectionFlag(messages);
   };
 
-  updateOverallSelectionFlag(messages) {
-    const statuses = messages.map((message) => message.selected);
-    const allSelected = statuses.every((status) => status);
-    const allUnselected = statuses.every((status) => !status);
-    if (allSelected) this.setState({ selection: flag.all, disabled: false });
-    else if (allUnselected)
-      this.setState({ selection: flag.none, disabled: true });
-    else this.setState({ selection: flag.some, disabled: false });
-  }
+  toggleStarring = (id) => {
+    const messages = [...this.state.messages];
+    const message = messages.find((message) => message.id === id);
+    message.starred = !message.starred;
+
+    this.setState({ messages: messages });
+  };
 
   markAsRead = () => {
     const messages = [...this.state.messages];
+
     messages.forEach((message) => {
-      if (message.selected && !message.read) message.read = true;
+      if (message.selected && !message.read) {
+        message.read = true;
+      }
     });
+
     this.setState({ messages: messages });
     this.countUnreadMessages(messages);
   };
 
   markAsUnread = () => {
     const messages = [...this.state.messages];
+
     messages.forEach((message) => {
-      if (message.selected && message.read) message.read = false;
+      if (message.selected && message.read) {
+        message.read = false;
+      }
     });
+
     this.setState({ messages: messages });
     this.countUnreadMessages(messages);
   };
 
-  deleteMessage = () => {
-    const messages = [...this.state.messages];
-    const retainedMessages = messages.filter((message) => !message.selected);
-    this.setState({ messages: retainedMessages });
-    this.updateOverallSelectionFlag(retainedMessages);
-    this.countUnreadMessages(retainedMessages);
-  };
-
-  countUnreadMessages(messages) {
-    const unreadMessages = messages.filter((message) => !message.read);
-    const unreadCount = unreadMessages.length;
-    this.setState({ unreadCount: unreadCount });
-    if (unreadCount === 1) this.setState({ plural: null });
-    else this.setState({ plural: "s" });
-  }
-
   applyLabel = (e) => {
     const labelToAdd = e.target.value;
     const messages = [...this.state.messages];
+
     messages.forEach((message) => {
-      if (message.selected && !message.labels.includes(labelToAdd))
+      if (message.selected && !message.labels.includes(labelToAdd)) {
         message.labels.push(labelToAdd);
+      }
     });
-    e.target.selectedIndex = 0;
+
     this.setState({ messages: messages });
+    e.target.selectedIndex = 0;
   };
 
   removeLabel = (e) => {
     const labelToRemove = e.target.value;
     const messages = [...this.state.messages];
+
     messages.forEach((message) => {
       if (message.selected && message.labels.includes(labelToRemove)) {
         const retainedLabels = message.labels.filter(
@@ -120,8 +134,19 @@ class App extends Component {
         message.labels = retainedLabels;
       }
     });
-    e.target.selectedIndex = 0;
+
     this.setState({ messages: messages });
+    e.target.selectedIndex = 0;
+  };
+
+  deleteMessage = () => {
+    const messages = [...this.state.messages];
+    const retainedMessages = messages.filter((message) => !message.selected);
+
+    this.setState({ messages: retainedMessages });
+
+    this.updateOverallSelectionFlag(retainedMessages);
+    this.countUnreadMessages(retainedMessages);
   };
 
   render = () => (
